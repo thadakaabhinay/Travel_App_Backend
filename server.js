447442fa83e7a672aeefb1d423a2ee58bdc11b90
Abhinay <thadakaabhinay@gmail.com>
2024-08-03 11:48:09 +0530

    fixed resolved cors issue

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express=require("express");
 const dotenv=require("dotenv");
+const cors=require("cors");
 
 dotenv.config();
 
@@ -19,14 +20,14 @@ const mongoose=require("mongoose");
 const connectDB=require("./config/dbconfig");
 
 const app=express();
-
+app.use(cors())
 app.use(express.json());
 connectDB();
 
 const PORT=3500;
 
 app.get("/",(req,res)=>{
-    res.send("Hello World");
+    res.send("Hello World, I Am Abhinay");
 })
 
 app.use("/api/hoteldata",hotelDataAddedToDBRouter);
